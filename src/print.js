ad7a22aeaa6bf132d5b0aa7b938acf02902776ce
Shuxiu <dongshuxiu@blued.com>
2019-11-14 19:34:10 +0800

    update:add server

--- src/print.js
+++ src/print.js
@@ -1,3 +1,6 @@
 export default function printMe() {
+
   console.log('I get called from print.js!');
+  // 测试inline-source-map
+  //cosnole.error('I get called from print.js!');
 }
--- webpack.config.js
+++ webpack.config.js
@@ -16,6 +16,13 @@ module.exports = {
           title: 'Output Management'
         })
   ],
+  // a.js b.js c.js整合到bundle.js,方便定位是哪一个打包的
+  devtool: 'inline-source-map',
+  // webpack-server:告知webpack-dev-server,在localhost:8080下建立
+  // 服务,将dist目录下的文件，作为可访问文件。
+  devServer: {
+    contentBase: './dist'
+  },
   output: {
     // filename: 'bundle.js',
     filename: '[name].bundle.js',
